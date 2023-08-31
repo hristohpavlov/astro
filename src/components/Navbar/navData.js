7aa96b0d2cd819c19db5b07efe92f843b65b3b83
hristohpavlov <hristohpavlov@gmail.com>
2023-09-01 01:52:37 +0300

    First commit

---
+++ src/components/Navbar/navData.js
@@ -0,0 +1,45 @@
+export const navLinks = [
+  {
+    label: '',
+    pathname: '',
+  },
+  // {
+  //   label: 'Projects',
+  //   pathname: '/#project-1',
+  // },
+  // {
+  //   label: 'Details',
+  //   pathname: '/#details',
+  // },
+  // {
+  //   label: 'Resume',
+  //   pathname: '/resume',
+  // },
+  // {
+  //   label: 'Contact',
+  //   pathname: '/contact',
+  // },
+];
+
+export const socialLinks = [
+  {
+    label: 'Twitter',
+    url: 'https://twitter.com/hristohpavlov',
+    icon: 'twitter',
+  },
+  // {
+  //   label: 'Twitter',
+  //   url: 'https://twitter.com/hristohpavlov',
+  //   icon: 'twitter',
+  // },
+  // {
+  //   label: 'Linkedin',
+  //   url: 'https://www.linkedin.com/in/hristo-h-pavlov/',
+  //   icon: 'linkedin',
+  // },
+  // {
+  //   label: 'Github',
+  //   url: 'https://github.com/hristohpavlov',
+  //   icon: 'github',
+  // },
+];
